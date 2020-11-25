a1b40528c937ca5603f716b4afcbf451ddbe66bd
yonghao.he@aliyun.com <scu0743111019>
2020-11-25 14:50:31 +0800

    normalize

--- data_pipeline/augmentation/augmentation_pipeline.py
+++ data_pipeline/augmentation/augmentation_pipeline.py
@@ -16,26 +16,33 @@ __all__ = ['typical_coco_train_pipeline',
 random_horizon_flip = HorizontalFlip(p=0.5)
 
 # CAUTION: normalize may vary along with different pretrained backbones
-normalize = Normalize(
+caffe_imagenet_normalize = Normalize(
     mean=(102.9801, 115.9465, 122.7717),
     std=(1.0, 1.0, 1.0),
     max_pixel_value=1.0,
     p=1.0
 )
 
+simple_normalize = Normalize(
+    mean=(0.5, 0.5, 0.5),
+    std=(0.5, 0.5, 0.5),
+    max_pixel_value=255.0,
+    p=1.0
+)
+
 bbox_param = BboxParams(format='coco', label_fields=['bbox_labels'])  # x,y,w,h
 train_pipeline_with_bboxes = Compose([random_horizon_flip,
-                                      normalize],
+                                      caffe_imagenet_normalize],
                                      bbox_params=bbox_param,
                                      p=1.)
 train_pipeline_without_bboxes = Compose([random_horizon_flip,
-                                         normalize],
+                                         caffe_imagenet_normalize],
                                         p=1.)
 
-val_pipeline_with_bboxes = Compose([normalize],
+val_pipeline_with_bboxes = Compose([caffe_imagenet_normalize],
                                    bbox_params=bbox_param,
                                    p=1.)
-val_pipeline_without_bboxes = Compose([normalize],
+val_pipeline_without_bboxes = Compose([caffe_imagenet_normalize],
                                       p=1.)
 
 
@@ -53,15 +60,30 @@ def typical_coco_val_pipeline(sample):
         return val_pipeline_without_bboxes(**sample)
 
 
+widerface_train_pipeline_with_bboxes = Compose([random_horizon_flip,
+                                                simple_normalize],
+                                               bbox_params=bbox_param,
+                                               p=1.)
+widerface_train_pipeline_without_bboxes = Compose([random_horizon_flip,
+                                                   simple_normalize],
+                                                  p=1.)
+
+widerface_val_pipeline_with_bboxes = Compose([simple_normalize],
+                                             bbox_params=bbox_param,
+                                             p=1.)
+widerface_val_pipeline_without_bboxes = Compose([simple_normalize],
+                                                p=1.)
+
+
 def simple_widerface_train_pipeline(sample):
     if 'bboxes' in sample:
-        return train_pipeline_with_bboxes(**sample)
+        return widerface_train_pipeline_with_bboxes(**sample)
     else:
-        return train_pipeline_without_bboxes(**sample)
+        return widerface_train_pipeline_without_bboxes(**sample)
 
 
 def simple_widerface_val_pipeline(sample):
     if 'bboxes' in sample:
-        return val_pipeline_with_bboxes(**sample)
+        return widerface_val_pipeline_with_bboxes(**sample)
     else:
-        return val_pipeline_without_bboxes(**sample)
+        return widerface_val_pipeline_without_bboxes(**sample)
